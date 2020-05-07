CORE-UPM/quiz_2020

Step 6.1: Quiz with Attachment.

controllers/quiz.js
@@ -1,8 +1,10 @@
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 const {models} = require("../models");
+const attHelper = require("../helpers/attachments");
 
 const paginate = require('../helpers/paginate').paginate;
+
 
 // Autoload el quiz asociado a :quizId
 exports.load = async (req, res, next, quizId) => {
@@ -10,6 +12,7 @@ exports.load = async (req, res, next, quizId) => {
     try {
         const quiz = await models.Quiz.findByPk(quizId, {
             include: [
+                {model: models.Attachment, as: 'attachment'},
                 {model: models.User, as: 'author'}
             ]
         });
@@ -28,7 +31,7 @@ exports.load = async (req, res, next, quizId) => {
 // MW that allows actions only if the user logged in is admin or is the author of the quiz.
 exports.adminOrAuthorRequired = (req, res, next) => {
 
-    const isAdmin  = !!req.loginUser.isAdmin;
+    const isAdmin = !!req.loginUser.isAdmin;
     const isAuthor = req.load.quiz.authorId === req.loginUser.id;
 
     if (isAdmin || isAuthor) {
@@ -55,9 +58,9 @@ exports.index = async (req, res, next) => {
     // Search:
     const search = req.query.search || '';
     if (search) {
-        const search_like = "%" + search.replace(/ +/g,"%") + "%";
+        const search_like = "%" + search.replace(/ +/g, "%") + "%";
 
-        countOptions.where.question = { [Op.like]: search_like };
+        countOptions.where.question = {[Op.like]: search_like};
         findOptions.where.question = { [Op.like]: search_like };
     }
 
@@ -89,12 +92,16 @@ exports.index = async (req, res, next) => {
 
         findOptions.offset = items_per_page * (pageno - 1);
         findOptions.limit = items_per_page;
-        findOptions.include = [{model: models.User, as: 'author'}];
+        findOptions.include = [
+                {model: models.Attachment, as: 'attachment'},
+                {model: models.User, as: 'author'}
+            ];
 
         const quizzes = await models.Quiz.findAll(findOptions);
         res.render('quizzes/index.ejs', {
             quizzes,
             search,
+            attHelper,
             title
         });
     } catch (error) {
@@ -108,7 +115,10 @@ exports.show = (req, res, next) => {
 
     const {quiz} = req.load;
 
-    res.render('quizzes/show', {quiz});
+    res.render('quizzes/show', {
+        quiz,
+        attHelper
+    });
 };
 
 
@@ -127,20 +137,29 @@ exports.new = (req, res, next) => {
 exports.create = async (req, res, next) => {
 
     const {question, answer} = req.body;
-
     const authorId = req.loginUser && req.loginUser.id || 0;
 
-    let quiz = models.Quiz.build({
-        question,
-        answer,
-        authorId
-    });
+    let quiz = models.Quiz.build({question, answer, authorId});
 
     try {
         // Saves only the fields question and answer into the DDBB
         quiz = await quiz.save({fields: ["question", "answer", "authorId"]});
         req.flash('success', 'Quiz created successfully.');
-        res.redirect('/quizzes/' + quiz.id);
+
+        try {
+            if (!req.file) {
+                req.flash('info', 'Quiz without attachment.');
+                return;
+            }
+
+            // Create the quiz attachment
+            await createQuizAttachment(req, quiz);
+
+        } catch (error) {
+            req.flash('error', 'Failed to create attachment: ' + error.message);
+        } finally {
+            res.redirect('/quizzes/' + quiz.id);
+        }
     } catch (error) {
         if (error instanceof Sequelize.ValidationError) {
             req.flash('error', 'There are errors in the form:');
@@ -148,8 +167,39 @@ exports.create = async (req, res, next) => {
             res.render('quizzes/new', {quiz});
         } else {
             req.flash('error', 'Error creating a new Quiz: ' + error.message);
-            next(error);
+            next(error)
         }
+    } finally {
+        // delete the file uploaded to ./uploads by multer.
+        if (req.file) {
+            attHelper.deleteLocalFile(req.file.path);
+        }
+    }
+};
+
+// Aux function to upload req.file to cloudinary, create an attachment with it, and
+// associate it with the gien quiz.
+// This function is called from the create an update middleware. DRY.
+const createQuizAttachment = async (req, quiz) => {
+
+    // Save the attachment into Cloudinary
+    const uploadResult = await attHelper.uploadResource(req);
+
+    let attachment;
+    try {
+        // Create the new attachment into the data base.
+        attachment = await models.Attachment.create({
+            public_id: uploadResult.public_id,
+            url: uploadResult.url,
+            filename: req.file.originalname,
+            mime: req.file.mimetype
+        });
+        await quiz.setAttachment(attachment);
+        req.flash('success', 'Attachment saved successfully.');
+    } catch (error) { // Ignoring validation errors
+        req.flash('error', 'Failed linking attachment: ' + error.message);
+        attHelper.deleteResource(uploadResult.public_id);
+        attachment && attachment.destroy();
     }
 };
 
@@ -175,7 +225,30 @@ exports.update = async (req, res, next) => {
     try {
         await quiz.save({fields: ["question", "answer"]});
         req.flash('success', 'Quiz edited successfully.');
-        res.redirect('/quizzes/' + quiz.id);
+
+        try {
+            if (req.body.keepAttachment) return; // Don't change the attachment.
+
+            // Delete old attachment.
+            if (quiz.attachment) {
+                attHelper.deleteResource(quiz.attachment.public_id);
+                await quiz.attachment.destroy();
+                await quiz.setAttachment();
+            }
+
+            if (!req.file) {
+                req.flash('info', 'Quiz without attachment.');
+                return;
+            }
+
+            // Create the quiz attachment
+            await createQuizAttachment(req, quiz);
+
+        } catch (error) {
+            req.flash('error', 'Failed saving the new attachment: ' + error.message);
+        } finally {
+            res.redirect('/quizzes/' + quiz.id);
+        }
     } catch (error) {
         if (error instanceof Sequelize.ValidationError) {
             req.flash('error', 'There are errors in the form:');
@@ -185,6 +258,11 @@ exports.update = async (req, res, next) => {
             req.flash('error', 'Error editing the Quiz: ' + error.message);
             next(error);
         }
+    } finally {
+        // delete the file uploaded to ./uploads by multer.
+        if (req.file) {
+            attHelper.deleteLocalFile(req.file.path);
+        }
     }
 };
 
@@ -192,11 +270,22 @@ exports.update = async (req, res, next) => {
 // DELETE /quizzes/:quizId
 exports.destroy = async (req, res, next) => {
 
+    const attachment = req.load.quiz.attachment;
+
+    // Delete the attachment
+    if (attachment) {
+        try {
+            attHelper.deleteResource(attachment.public_id);
+        } catch (error) {
+        }
+    }
+
     try {
         await req.load.quiz.destroy();
+        attachment && await attachment.destroy();
         req.flash('success', 'Quiz deleted successfully.');
         res.redirect('/goback');
-    } catch (error) {
+    }  catch(error) {
         req.flash('error', 'Error deleting the Quiz: ' + error.message);
         next(error);
     }
@@ -204,32 +293,33 @@ exports.destroy = async (req, res, next) => {
 
 
 // GET /quizzes/:quizId/play
-    exports.play = (req, res, next) => {
+exports.play = (req, res, next) => {
 
-        const {query} = req;
+    const {query} = req;
         const {quiz} = req.load;
 
-        const answer = query.answer || '';
+    const answer = query.answer || '';
 
-        res.render('quizzes/play', {
-            quiz,
-            answer
-        });
-    };
+    res.render('quizzes/play', {
+        quiz,
+        answer,
+        attHelper
+    });
+};
 
 
 // GET /quizzes/:quizId/check
-    exports.check = (req, res, next) => {
+exports.check = (req, res, next) => {
 
-        const {query} = req;
+    const {query} = req;
         const {quiz} = req.load;
 
-        const answer = query.answer || "";
-        const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
+    const answer = query.answer || "";
+    const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
 
-        res.render('quizzes/result', {
-            quiz,
-            result,
-            answer
-        });
-    };
+    res.render('quizzes/result', {
+        quiz,
+        result,
+        answer
+    });
+};

helpers/attachments.js
@@ -1,0 +1,93 @@
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
+
+/**
+ * Create a promise to upload a new file.
+ *
+ * If the file can be uploaded then the promise is satisfied and returns the public_id and
+ * the url of the uploaded resource.
+ * If the file can not be uploaded, the promise is rejected.
+ *
+ * The file to upload is req.file.path
+ *
+ * @return Returns a Promise.
+ */
+exports.uploadResource = req => {
+    return new Promise(function (resolve, reject) {
+
+        const salt = Math.round((new Date().valueOf() * Math.random())) + '';
+        const public_id = crypto.createHmac('sha256', salt).update(req.file.path).digest('hex');
+
+        const urlPrefix = req.protocol + "://" + req.headers.host;
+        const url = urlPrefix + path.join("/uploads", public_id);
+
+        const destination = path.join("public", "uploads", public_id);
+
+        fs.copyFile(req.file.path, destination, fs.constants.COPYFILE_EXCL, function (error) {
+
+            if (error) {
+                reject(error);
+            } else {
+                resolve({
+                    public_id: public_id,
+                    url: url
+                });
+            }
+        });
+    });
+};
+
+
+
+
+/**
+ * Deletes a saved file.
+ */
+exports.deleteResource = function (public_id) {
+
+    const destination = path.join("public", "uploads", public_id);
+
+    // Delete from local file system.
+    fs.unlink(destination, function (error) {
+        console.log("Error deleting attachment file from local file system:", error);
+    });
+};
+
+
+/**
+ * Returns HTML img element for the given URL.
+ *
+ * @param url         URL of the image.
+ * @returns {string}  String with the <img> element.
+ */
+exports.image = url => {
+
+    return `<img class="attachment" src="${url}" alt="Image">`;
+};
+
+
+/**
+ * Returns HTML video element for the given URL.
+ *
+ * @param url         URL of the video.
+ * @returns {string}  String with the <video> element.
+ */
+exports.video =  url => {
+
+    return `<video class="attachment" alt="Video"><source src="${url}" type="video/mp4"></video>`;
+};
+
+/**
+ * Delete a file in the local file system.
+ *
+ * @param path Path to the file to delete.
+ */
+exports.deleteLocalFile = path => {
+    fs.unlink(path, err => {
+        if (err) {
+            console.log(`Error deleting file: ${path} >> ${err}`);
+        }
+    });
+};

models/attachment.js
@@ -1,0 +1,41 @@
+
+const {Model} = require('sequelize');
+
+// Definition of the Attachment model:
+
+module.exports = (sequelize, DataTypes) => {
+
+    class Attachment extends Model {}
+
+    Attachment.init({
+            public_id: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: {msg: "public_id can not be empty."}
+                }
+            },
+            url: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: {msg: "url can not be empty."}
+                }
+            },
+            filename: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: {msg: "filename can not be empty."}
+                }
+            },
+            mime: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: {msg: "mime can not be empty."}
+                }
+            }
+        }, {
+            sequelize
+        }
+    );
+
+    return Attachment;
+};

CORE-UPM/Quiz_2020_Examen_20180529

controllers/quiz.js
@@ -1,8 +1,10 @@
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 const {models} = require("../models");
+const attHelper = require("../helpers/attachments");
 
 const paginate = require('../helpers/paginate').paginate;
+
 
 // Autoload el quiz asociado a :quizId
 exports.load = async (req, res, next, quizId) => {
@@ -10,6 +12,7 @@ exports.load = async (req, res, next, quizId) => {
     try {
         const quiz = await models.Quiz.findByPk(quizId, {
             include: [
+                {model: models.Attachment, as: 'attachment'},
                 {model: models.User, as: 'author'}
             ]
         });
@@ -28,7 +31,7 @@ exports.load = async (req, res, next, quizId) => {
 // MW that allows actions only if the user logged in is admin or is the author of the quiz.
 exports.adminOrAuthorRequired = (req, res, next) => {
 
-    const isAdmin  = !!req.loginUser.isAdmin;
+    const isAdmin = !!req.loginUser.isAdmin;
     const isAuthor = req.load.quiz.authorId === req.loginUser.id;
 
     if (isAdmin || isAuthor) {
@@ -55,9 +58,9 @@ exports.index = async (req, res, next) => {
     // Search:
     const search = req.query.search || '';
     if (search) {
-        const search_like = "%" + search.replace(/ +/g,"%") + "%";
+        const search_like = "%" + search.replace(/ +/g, "%") + "%";
 
-        countOptions.where.question = { [Op.like]: search_like };
+        countOptions.where.question = {[Op.like]: search_like};
         findOptions.where.question = { [Op.like]: search_like };
     }
 
@@ -89,12 +92,16 @@ exports.index = async (req, res, next) => {
 
         findOptions.offset = items_per_page * (pageno - 1);
         findOptions.limit = items_per_page;
-        findOptions.include = [{model: models.User, as: 'author'}];
+        findOptions.include = [
+                {model: models.Attachment, as: 'attachment'},
+                {model: models.User, as: 'author'}
+            ];
 
         const quizzes = await models.Quiz.findAll(findOptions);
         res.render('quizzes/index.ejs', {
             quizzes,
             search,
+            attHelper,
             title
         });
     } catch (error) {
@@ -108,7 +115,10 @@ exports.show = (req, res, next) => {
 
     const {quiz} = req.load;
 
-    res.render('quizzes/show', {quiz});
+    res.render('quizzes/show', {
+        quiz,
+        attHelper
+    });
 };
 
 
@@ -127,20 +137,29 @@ exports.new = (req, res, next) => {
 exports.create = async (req, res, next) => {
 
     const {question, answer} = req.body;
-
     const authorId = req.loginUser && req.loginUser.id || 0;
 
-    let quiz = models.Quiz.build({
-        question,
-        answer,
-        authorId
-    });
+    let quiz = models.Quiz.build({question, answer, authorId});
 
     try {
         // Saves only the fields question and answer into the DDBB
         quiz = await quiz.save({fields: ["question", "answer", "authorId"]});
         req.flash('success', 'Quiz created successfully.');
-        res.redirect('/quizzes/' + quiz.id);
+
+        try {
+            if (!req.file) {
+                req.flash('info', 'Quiz without attachment.');
+                return;
+            }
+
+            // Create the quiz attachment
+            await createQuizAttachment(req, quiz);
+
+        } catch (error) {
+            req.flash('error', 'Failed to create attachment: ' + error.message);
+        } finally {
+            res.redirect('/quizzes/' + quiz.id);
+        }
     } catch (error) {
         if (error instanceof Sequelize.ValidationError) {
             req.flash('error', 'There are errors in the form:');
@@ -148,8 +167,39 @@ exports.create = async (req, res, next) => {
             res.render('quizzes/new', {quiz});
         } else {
             req.flash('error', 'Error creating a new Quiz: ' + error.message);
-            next(error);
+            next(error)
         }
+    } finally {
+        // delete the file uploaded to ./uploads by multer.
+        if (req.file) {
+            attHelper.deleteLocalFile(req.file.path);
+        }
+    }
+};
+
+// Aux function to upload req.file to cloudinary, create an attachment with it, and
+// associate it with the gien quiz.
+// This function is called from the create an update middleware. DRY.
+const createQuizAttachment = async (req, quiz) => {
+
+    // Save the attachment into Cloudinary
+    const uploadResult = await attHelper.uploadResource(req);
+
+    let attachment;
+    try {
+        // Create the new attachment into the data base.
+        attachment = await models.Attachment.create({
+            resource: uploadResult.resource,
+            url: uploadResult.url,
+            filename: req.file.originalname,
+            mime: req.file.mimetype
+        });
+        await quiz.setAttachment(attachment);
+        req.flash('success', 'Attachment saved successfully.');
+    } catch (error) { // Ignoring validation errors
+        req.flash('error', 'Failed linking attachment: ' + error.message);
+        attHelper.deleteResource(uploadResult.resource);
+        attachment && attachment.destroy();
     }
 };
 
@@ -175,7 +225,30 @@ exports.update = async (req, res, next) => {
     try {
         await quiz.save({fields: ["question", "answer"]});
         req.flash('success', 'Quiz edited successfully.');
-        res.redirect('/quizzes/' + quiz.id);
+
+        try {
+            if (req.body.keepAttachment) return; // Don't change the attachment.
+
+            // Delete old attachment.
+            if (quiz.attachment) {
+                attHelper.deleteResource(quiz.attachment.resource);
+                await quiz.attachment.destroy();
+                await quiz.setAttachment();
+            }
+
+            if (!req.file) {
+                req.flash('info', 'Quiz without attachment.');
+                return;
+            }
+
+            // Create the quiz attachment
+            await createQuizAttachment(req, quiz);
+
+        } catch (error) {
+            req.flash('error', 'Failed saving the new attachment: ' + error.message);
+        } finally {
+            res.redirect('/quizzes/' + quiz.id);
+        }
     } catch (error) {
         if (error instanceof Sequelize.ValidationError) {
             req.flash('error', 'There are errors in the form:');
@@ -185,6 +258,11 @@ exports.update = async (req, res, next) => {
             req.flash('error', 'Error editing the Quiz: ' + error.message);
             next(error);
         }
+    } finally {
+        // delete the file uploaded to ./uploads by multer.
+        if (req.file) {
+            attHelper.deleteLocalFile(req.file.path);
+        }
     }
 };
 
@@ -192,11 +270,22 @@ exports.update = async (req, res, next) => {
 // DELETE /quizzes/:quizId
 exports.destroy = async (req, res, next) => {
 
+    const attachment = req.load.quiz.attachment;
+
+    // Delete the attachment
+    if (attachment) {
+        try {
+            attHelper.deleteResource(attachment.resource);
+        } catch (error) {
+        }
+    }
+
     try {
         await req.load.quiz.destroy();
+        attachment && await attachment.destroy();
         req.flash('success', 'Quiz deleted successfully.');
         res.redirect('/goback');
-    } catch (error) {
+    }  catch(error) {
         req.flash('error', 'Error deleting the Quiz: ' + error.message);
         next(error);
     }
@@ -204,32 +293,33 @@ exports.destroy = async (req, res, next) => {
 
 
 // GET /quizzes/:quizId/play
-    exports.play = (req, res, next) => {
+exports.play = (req, res, next) => {
 
-        const {query} = req;
+    const {query} = req;
         const {quiz} = req.load;
 
-        const answer = query.answer || '';
+    const answer = query.answer || '';
 
-        res.render('quizzes/play', {
-            quiz,
-            answer
-        });
-    };
+    res.render('quizzes/play', {
+        quiz,
+        answer,
+        attHelper
+    });
+};
 
 
 // GET /quizzes/:quizId/check
-    exports.check = (req, res, next) => {
+exports.check = (req, res, next) => {
 
-        const {query} = req;
+    const {query} = req;
         const {quiz} = req.load;
 
-        const answer = query.answer || "";
-        const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
+    const answer = query.answer || "";
+    const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
 
-        res.render('quizzes/result', {
-            quiz,
-            result,
-            answer
-        });
-    };
+    res.render('quizzes/result', {
+        quiz,
+        result,
+        answer
+    });
+};

helpers/attachments.js
@@ -1,0 +1,93 @@
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
+
+/**
+ * Create a promise to upload a new file.
+ *
+ * If the file can be uploaded then the promise is satisfied and returns the resource and
+ * the url of the uploaded resource.
+ * If the file can not be uploaded, the promise is rejected.
+ *
+ * The file to upload is req.file.path
+ *
+ * @return Returns a Promise.
+ */
+exports.uploadResource = req => {
+    return new Promise(function (resolve, reject) {
+
+        const salt = Math.round((new Date().valueOf() * Math.random())) + '';
+        const resource = crypto.createHmac('sha256', salt).update(req.file.path).digest('hex');
+
+        const urlPrefix = req.protocol + "://" + req.headers.host;
+        const url = urlPrefix + path.join("/uploads", resource);
+
+        const destination = path.join("public", "uploads", resource);
+
+        fs.copyFile(req.file.path, destination, fs.constants.COPYFILE_EXCL, function (error) {
+
+            if (error) {
+                reject(error);
+            } else {
+                resolve({
+                    resource,
+                    url
+                });
+            }
+        });
+    });
+};
+
+
+
+
+/**
+ * Deletes a saved file.
+ */
+exports.deleteResource = resource => {
+
+    const destination = path.join("public", "uploads", resource);
+
+    // Delete from local file system.
+    fs.unlink(destination, function (error) {
+        console.log("Error deleting attachment file from local file system:", error);
+    });
+};
+
+
+/**
+ * Returns HTML img element for the given URL.
+ *
+ * @param url         URL of the image.
+ * @returns {string}  String with the <img> element.
+ */
+exports.image = url => {
+
+    return `<img class="attachment" src="${url}" alt="Image">`;
+};
+
+
+/**
+ * Returns HTML video element for the given URL.
+ *
+ * @param url         URL of the video.
+ * @returns {string}  String with the <video> element.
+ */
+exports.video =  url => {
+
+    return `<video class="attachment" alt="Video"><source src="${url}" type="video/mp4"></video>`;
+};
+
+/**
+ * Delete a file in the local file system.
+ *
+ * @param path Path to the file to delete.
+ */
+exports.deleteLocalFile = path => {
+    fs.unlink(path, err => {
+        if (err) {
+            console.log(`Error deleting file: ${path} >> ${err}`);
+        }
+    });
+};

models/attachment.js
@@ -1,0 +1,41 @@
+
+const {Model} = require('sequelize');
+
+// Definition of the Attachment model:
+
+module.exports = (sequelize, DataTypes) => {
+
+    class Attachment extends Model {}
+
+    Attachment.init({
+            resource: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: {msg: "resource can not be empty."}
+                }
+            },
+            url: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: {msg: "url can not be empty."}
+                }
+            },
+            filename: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: {msg: "filename can not be empty."}
+                }
+            },
+            mime: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: {msg: "mime can not be empty."}
+                }
+            }
+        }, {
+            sequelize
+        }
+    );
+
+    return Attachment;
+};