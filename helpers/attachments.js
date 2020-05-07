CORE-UPM/quiz_2020

Step 10.1: Save Attachments and Photos also in Cloudinary.

helpers/attachments.js
@@ -1,6 +1,16 @@
+const cloudinary = require('cloudinary');
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
+
+
+// Options for the files uploaded to Cloudinary
+const cloudinary_upload_options = {
+    async: false,
+    folder: "/core/quiz2020/attachments",
+    resource_type: "auto",
+    tags: ['core', 'iweb', 'cdps', 'quiz2020']
+};
 
 
 /**
@@ -10,11 +20,68 @@ const crypto = require('crypto');
  * the url of the uploaded resource.
  * If the file can not be uploaded, the promise is rejected.
  *
+ *  The file can be uploaded to: Cloudinary, or to the local file system.
+ *   - Cloudinary - If there exists the CLOUDINARY_URL environment variable.
+ *   - Local file system - If there does not exist the CLOUDINARY_URL environment variable.
+ *                         The file is saved into the public/uploads directory
+ *
  * The file to upload is req.file.path
  *
  * @return Returns a Promise.
  */
 exports.uploadResource = req => {
+
+    return new Promise(function (resolve, reject) {
+        if (!!process.env.CLOUDINARY_URL) {
+            resolve(uploadResourceToCloudinary(req));
+        } else {
+            resolve(uploadResourceToFileSystem(req));
+        }
+    });
+};
+
+
+/**
+ * Create a promise to upload a new file to Cloudinary.
+ *
+ * If the file can be uploaded then the promise is satisfied and returns the public_id and
+ * the url of the uploaded resource.
+ * If the file can not be uploaded, the promise is rejected.
+ *
+ * The file to upload is req.file.path
+ *
+ * @return Returns a Promise.
+ */
+exports.uploadResourceToCloudinary = req => {
+
+    return new Promise((resolve, reject) => {
+
+        cloudinary.v2.uploader.upload(
+            req.file.path,
+            cloudinary_upload_options,
+            (error, result) => {
+                if (!error) {
+                    resolve({public_id: result.public_id, url: result.secure_url});
+                } else {
+                    reject(error);
+                }
+            }
+        );
+    })
+};
+
+
+/**
+ * Create a promise to upload a new file to the local file system.
+ *
+ * If the file can be uploaded then the promise is satisfied and returns the public_id and
+ * the url of the uploaded resource.
+ * If the file can not be uploaded, the promise is rejected.
+ *
+ * @return Returns a Promise.
+ */
+const uploadResourceToFileSystem = req => {
+
     return new Promise(function (resolve, reject) {
 
         const salt = Math.round((new Date().valueOf() * Math.random())) + '';
@@ -47,12 +114,17 @@ exports.uploadResource = req => {
  */
 exports.deleteResource = function (public_id) {
 
-    const destination = path.join("public", "uploads", public_id);
+    if (!!process.env.CLOUDINARY_URL) {
+        // Delete from Cloudinary.
+        cloudinary.api.delete_resources(public_id);
+    } else {
+        const destination = path.join("public", "uploads", public_id);
 
-    // Delete from local file system.
-    fs.unlink(destination, function (error) {
-        console.log("Error deleting attachment file from local file system:", error);
-    });
+        // Delete from local file system.
+        fs.unlink(destination, function (error) {
+            console.log("Error deleting attachment file from local file system:", error);
+        });
+    }
 };
 
 

CORE-UPM/Quiz_2020_Examen_20180529

helpers/attachments.js
@@ -1,6 +1,16 @@
+const cloudinary = require('cloudinary');
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
+
+
+// Options for the files uploaded to Cloudinary
+const cloudinary_upload_options = {
+    async: false,
+    folder: "/core/quiz2020/attachments",
+    resource_type: "auto",
+    tags: ['core', 'iweb', 'cdps', 'quiz2020']
+};
 
 
 /**
@@ -10,11 +20,68 @@ const crypto = require('crypto');
  * the url of the uploaded resource.
  * If the file can not be uploaded, the promise is rejected.
  *
+ *  The file can be uploaded to: Cloudinary, or to the local file system.
+ *   - Cloudinary - If there exists the CLOUDINARY_URL environment variable.
+ *   - Local file system - If there does not exist the CLOUDINARY_URL environment variable.
+ *                         The file is saved into the public/uploads directory
+ *
  * The file to upload is req.file.path
  *
  * @return Returns a Promise.
  */
 exports.uploadResource = req => {
+
+    return new Promise(function (resolve, reject) {
+        if (!!process.env.CLOUDINARY_URL) {
+            resolve(uploadResourceToCloudinary(req));
+        } else {
+            resolve(uploadResourceToFileSystem(req));
+        }
+    });
+};
+
+
+/**
+ * Create a promise to upload a new file to Cloudinary.
+ *
+ * If the file can be uploaded then the promise is satisfied and returns the resource (public_id) and
+ * the url of the uploaded resource.
+ * If the file can not be uploaded, the promise is rejected.
+ *
+ * The file to upload is req.file.path
+ *
+ * @return Returns a Promise.
+ */
+const uploadResourceToCloudinary = req => {
+
+    return new Promise((resolve, reject) => {
+
+        cloudinary.v2.uploader.upload(
+            req.file.path,
+            cloudinary_upload_options,
+            (error, result) => {
+                if (!error) {
+                    resolve({resource: result.public_id, url: result.secure_url});
+                } else {
+                    reject(error);
+                }
+            }
+        );
+    })
+};
+
+
+/**
+ * Create a promise to upload a new file to the local file system.
+ *
+ * If the file can be uploaded then the promise is satisfied and returns the resource and
+ * the url of the uploaded resource.
+ * If the file can not be uploaded, the promise is rejected.
+ *
+ * @return Returns a Promise.
+ */
+const uploadResourceToFileSystem = req => {
+
     return new Promise(function (resolve, reject) {
 
         const salt = Math.round((new Date().valueOf() * Math.random())) + '';
@@ -47,12 +114,17 @@ exports.uploadResource = req => {
  */
 exports.deleteResource = resource => {
 
-    const destination = path.join("public", "uploads", resource);
+    if (!!process.env.CLOUDINARY_URL) {
+        // Delete from Cloudinary.
+        cloudinary.api.delete_resources(resource);
+    } else {
+        const destination = path.join("public", "uploads", resource);
 
-    // Delete from local file system.
-    fs.unlink(destination, function (error) {
-        console.log("Error deleting attachment file from local file system:", error);
-    });
+        // Delete from local file system.
+        fs.unlink(destination, function (error) {
+            console.log("Error deleting attachment file from local file system:", error);
+        });
+    }
 };
 
 